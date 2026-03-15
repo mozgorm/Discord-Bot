3dc7fce88e0b606bc382551f2082a5e9396f696b
mozgorm <mozgorm@gmail.com>
2026-03-15 09:11:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import discord
 from discord.ext import commands
-
 import os
-TOKEN = os.getenv("DISCORD_TOKEN")
 
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 WATCH_USER_ID = 1447561484040146985
 SOURCE_CHANNEL_ID = 1403284630039433238
@@ -20,39 +19,31 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-
     if message.author.bot:
         return
 
     if message.author.id == WATCH_USER_ID and message.channel.id == SOURCE_CHANNEL_ID:
-
         log_channel = bot.get_channel(LOG_CHANNEL_ID)
-
         if log_channel:
-
             embed = discord.Embed(
                 title="Message Logged",
                 description=message.content,
                 color=0x2f3136
             )
-
             embed.set_author(
                 name=f"{message.author} ({message.author.id})",
                 icon_url=message.author.display_avatar.url
             )
-
             embed.add_field(
                 name="Channel",
                 value=message.channel.mention,
                 inline=True
             )
-
             embed.add_field(
                 name="Message ID",
                 value=message.id,
                 inline=True
             )
-
             embed.set_footer(text="Message Logger")
             embed.timestamp = message.created_at
 
